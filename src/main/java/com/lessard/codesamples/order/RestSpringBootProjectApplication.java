38c947ab49f6d257c15a2a39b077d576e2ab3f80
fdlessard <fdlessard@gmail.com>
2016-08-20 20:02:50 -0400

    Initial commit

---
+++ src/main/java/com/lessard/codesamples/order/RestSpringBootProjectApplication.java
@@ -0,0 +1,12 @@
+package com.lessard.codesamples.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lessard/codesamples/order/RestSpringBootProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lessard.codesamples.order;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestSpringBootProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
